48120fc0e1f41b3c8d1775d529d25afadb2adbc4
scheuerkev <scheuer.kevin@gmail.com>
2022-03-11 18:23:20 +0100

    End of original design integration

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,27 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.render("home");
+  res.render("pages/home");
+});
+
+router.get("/prestation", (req, res) => {
+  res.render("pages/prestation");
+});
+
+router.get("/portfolio", (req, res) => {
+  res.render("pages/portfolio");
+});
+
+router.get("/stack", (req, res) => {
+  res.render("pages/stack");
+});
+
+router.get("/blog", (req, res) => {
+  res.render("pages/blog");
+});
+
+router.get("/contact", (req, res) => {
+  res.render("pages/contact");
 });
 
 module.exports = router;
