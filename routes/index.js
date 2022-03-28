95059339b8c64913565cdc860eff7b10fba69e7f
scheuerkev <scheuer.kevin@gmail.com>
2022-03-28 22:33:13 +0200

    Maintenance version

--- routes/index.js
+++ routes/index.js
@@ -5,28 +5,8 @@ const message = require("./messages.route");
 router.use("/blog", blog);
 router.use("/message", message);
 
-router.get("/", (req, res) => {
-  res.render("pages/home");
-});
-
-router.get("/prestation", (req, res) => {
-  res.render("pages/prestation");
-});
-
-router.get("/portfolio", (req, res) => {
-  res.render("pages/portfolio");
-});
-
-router.get("/pourquoi", (req, res) => {
-  res.render("pages/pourquoi");
-});
-
-router.get("/contact", (req, res) => {
-  res.render("pages/contact");
-});
-
 router.get("*", (req, res) => {
-  res.render("pages/404");
+  res.render("pages/maintenance");
 });
 
 module.exports = router;
