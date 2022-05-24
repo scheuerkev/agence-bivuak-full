4a304b5d9bba3daae443009ab97a7a8292aefd40
scheuerkev <scheuer.kevin@gmail.com>
2022-05-24 18:03:25 +0200

    Handling with multer

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const errorHandler = require("errorhandler");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
 const path = require("path");
 const index = require("./routes");
 require("./database");
@@ -21,6 +22,12 @@ app.use(morgan("short"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(mongoSanitize({
+  replaceWith: '_',
+  onSanitize: ({req, key}) => {
+    console.warn(`This request[${key}] is sanitized`, req);
+  }
+}));
 app.use(index);
 
 if (process.env.NODE_ENV === "development") {
