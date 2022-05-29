7c500a05f0c601df7494466e367efe60de91942e
scheuerkev <scheuer.kevin@gmail.com>
2022-05-29 09:18:06 +0200

    Solving indexation problem

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const blog = require("./blog");
 const message = require("./messages.route");
 
-router.use("/blog", blog);
+//router.use("/blog", blog);
 router.use("/message", message);
 
 router.get("/mentions-legales", (req, res) => {
